195b252b5f4c26e94bd4bb9140f861b898d3e898
vagrant <vagrant@ubuntu1904.localdomain>
2019-12-11 06:27:33 +0000

    Agregando cleaned.artworks

--- MOMA/sql/to_cleaned.sql
+++ MOMA/sql/to_cleaned.sql
@@ -13,3 +13,40 @@ select
 );
 
 create index cleaned_artists_constituent_id on cleaned.artists (constituent_id);
+
+drop table if exists cleaned.artworks;
+
+create table cleaned.artworks as(
+
+select
+  "ObjectID"::int as object_id,
+  "Artist"::varchar as artist,
+  "Title"::varchar as title,
+  "ConstituentID" as constituent_id,
+  "Nationality"::varchar as nationality,
+  "BeginDate" as begin_date,
+  "EndDate" as end_date,
+  "Gender"::varchar as gender,
+  "Date" as date,
+  "Medium"::varchar as medium,
+  "CreditLine"::varchar as credit_line,
+  "Classification"::varchar as classification,
+  "Department"::varchar as department,
+  "DateAcquired" as date_acquired,
+  "Cataloged"::char as cataloged,
+  "URL"::varchar as url,
+  "ThumbnailURL" as thumbnail_url,
+  "Circumference (cm)" as circumference_cm,
+  "Depth (cm)" as depth_cm,
+  "Diameter (cm)" as diameter_cm,
+  "Height (cm)" as height_cm,
+  "Length (cm)" as length_cm,
+  "Weight (kg)" as weight_kg,
+  "Width (cm)" as width_cm,
+  "Seat Height (cm)" as seat_height_cm,
+  "Duration (sec.)" as duration_sec
+  from raw.Artworks
+);
+
+create index cleaned_artworks_object_id on cleaned.artworks (object_id);
+create index cleaned_artworks_constituent_id on cleaned.artworks (constituent_id);
